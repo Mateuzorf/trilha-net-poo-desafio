dc1d8292835964596e1ef5bc727cc2b66a709c9a
Mateuzorf <167330430+Mateuzorf@users.noreply.github.com>
2024-10-31 01:30:35 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("12333","modelos","2222222", 34);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Zap");
